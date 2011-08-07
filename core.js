f29820dd095355890045330be5c53edcb218fe49
joseferminj <joseferminj@gmail.com>
2011-08-07 20:28:14 +0200

    Creates a simple layout that contains a toolbar that shows a few menu options: Matches, Completed. Each menu option is a button the has associated panel

--- core.js
+++ core.js
@@ -1,2 +1,4 @@
 goog.addDependency("../cljs/core.js", ['cljs.core'], ['goog.string', 'goog.string.StringBuffer', 'goog.object', 'goog.array']);
-goog.addDependency("../core.js", ['playing_cljs.core'], ['cljs.core', 'goog.dom', 'goog.object', 'goog.events.Event', 'goog.events.EventType', 'goog.ui.ColorButton', 'goog.ui.Tab', 'goog.ui.TabBar']);
+goog.addDependency("../clojure/string.js", ['clojure.string'], ['cljs.core', 'goog.string', 'goog.string.StringBuffer']);
+goog.addDependency("../playing_cljs/dom-helpers.js", ['playing_cljs.dom_helpers'], ['cljs.core', 'clojure.string', 'goog.dom']);
+goog.addDependency("../playing_cljs/core.js", ['playing_cljs.core'], ['cljs.core', 'goog.debug.FancyWindow', 'goog.events', 'playing_cljs.dom_helpers', 'goog.ui.ToolbarButton', 'goog.debug.Logger', 'goog.events.EventType', 'goog.ui.Toolbar', 'goog.object', 'goog.debug']);
--- src/playing_cljs/core.cljs
+++ src/playing_cljs/core.cljs
@@ -1,80 +1,52 @@
 (ns playing-cljs.core
   (:require
-   [goog.dom :as dom]
+   [playing-cljs.dom-helpers :as dom]
    [goog.object :as goog-object]
-   [goog.events.Event :as goog-event]
-   [goog.events.EventType :as goog-event-type]
-   [goog.ui.ColorButton :as color-button]
-   [goog.ui.Tab :as gtab]
-   [goog.ui.TabBar :as gtabb]))      
+   [goog.events :as events]
+   [goog.events.EventType :as event-type]
+   [goog.ui.Toolbar :as gtoolbar]
+   [goog.ui.ToolbarButton :as gtbutton]
+   [goog.debug :as debug]
+   [goog.debug.FancyWindow :as fancy-window]
+   [goog.debug.Logger :as glogger]))      
 
 ;; -------------------------= State =-----------------------
 (def database-connected? (atom false))
 (def messaging-connected? (atom false))
 
-;; --------------------------= Widgets =-----------------------------
-(def database-button (doto (goog.ui.ColorButton. "Database")
-                 (.setTooltip "Database Connection Status")
-                 (.setValue "red")))
+(def log (glogger/getLogger "core.cljs"))
 
-(def messaging-button (doto (goog.ui.ColorButton. "Messaging")
-                        (.setTooltip "Messaging Connection Status")
-                        (.setValue "red")))
+(defn switch-panel [target]
+  (.info log (str "Execution swich panel with " target))
+  (.. dom/get-element "taskList" getStyle (setDisplay "none"))
+  (.. dom/get-element "completedTaskList" getStyle (setDisplay "none"))
+  (.. dom/get-element "deletedTaskList" getStyle (setDisplay "none"))
+  (.. dom/get-element "settings" getStyle (setDisplay "none"))
+  (.. dom/get-element target getStyle (setDisplay "block")))
 
-(def tabbar (goog.ui.TabBar.))
+(defn attach-toolbar-button [toolbar label tooltip target]
+  (let [button (doto (goog.ui.ToolbarButton. label)
+                 (.setTooltip tooltip))]
+    (.addChild toolbar button true)
+    (events/listen (. button (getContentElement)) event-type/CLICK (partial switch-panel target))))
 
-;; -----------------------= Control =---------------------
-;; Event handling for the UI
-(def events (.getValues goog.object goog.ui.Component/EventType))
-
-(defn handle-tab-select [tabbar e]
-  (let [tab (.target e)
-        content (.getElement goog.dom (str (. tabbar (getId)) "_content"))]
-    (.setTextContent goog.dom content (. tab (getCaption)))))
-
-(defn button-color
-  "Associate a colour with the states"
-  [x]
-  (if x "green" "red"))
-
-(defn toggle-state!
-  "Simulate trivial connect/disconnect functionality for the buttions"
-  [x]
-  (swap! x not))
-
-(defn handle-button-push [s e]
-  (.setValue (.target e)
-             (button-color (toggle-state! s))))
-
+(defn attach-toolbar [container]
+  (let [toolbar (goog.ui.Toolbar.)]
+    (attach-toolbar-button toolbar "Matches" "List currently open matches", "taskList")
+    (attach-toolbar-button toolbar "Completed" "List completd matches" "completedTaskList")
+    (attach-toolbar-button toolbar "Trash" "List deleted matches" "deletedTaskList")
+    (attach-toolbar-button toolbar "Settins" "Settings" "settings")
+    (.render toolbar container)))
 ;; ---------------------= MAIN =------------------------
 ;; Main entry function
 (defn ^:export main []
+  (doto (goog.debug.FancyWindow. "main")
+    (.setEnabled true)
+    (.init))
+
+  (.info log "Starting execution")
   ;; Populate a DOM via decoration.
-  (.decorate tabbar (.getElement goog.dom "top"))
-
-  ;; Populate a DOM via rendering.
-  (.render database-button (.getElement goog.dom "buttons"))
-  (.render messaging-button (.getElement goog.dom "buttons"))
-
-  ;; Wire up the events
-  ;;  The tabbar event
-  (.listen goog.events
-           tabbar
-           goog.ui.Component.EventType/SELECT
-           (partial handle-tab-select tabbar))
-
-  ;;  The database button
-  (.listen goog.events
-           database-button
-           goog.ui.Component.EventType/ACTION
-           (partial handle-button-push
-                    database-connected?))
-
-  ;; The messaging button
-  (.listen goog.events
-           messaging-button
-           goog.ui.Component.EventType/ACTION
-           (partial handle-button-push
-                    messaging-connected?)))   
+  (attach-toolbar (dom/get-element "menu"))
+  )
 
 
